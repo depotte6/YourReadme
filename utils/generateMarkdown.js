967598c8401c3d288be13074e1a45950998e11ea
depotte6 <depotte6@gmail.com>
2021-11-03 17:09:21 -0400

    adding set up for rendering

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if (!license) {
     return ''
   }
-  return `${license}`;
+  return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
 };
 
 // TODO: Create a function that returns the license link
@@ -18,11 +18,36 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  }
+  return `## [License](#table-of-contents)
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  *[Contributiong](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+
+
+
+
 
 `;
 }
